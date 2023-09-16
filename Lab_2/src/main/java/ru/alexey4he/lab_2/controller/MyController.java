f63b66177265bed4997ee41da093a2e34a71e269
«Alexey <alexey4he@yandex.ru>
2023-09-16 17:51:47 +0500

    Init Lab - 2 directory.

---
+++ Lab_2/src/main/java/ru/alexey4he/lab_2/controller/MyController.java
@@ -0,0 +1,28 @@
+package ru.alexey4he.lab_2.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.alexey4he.lab_2.model.Request;
+import ru.alexey4he.lab_2.model.Response;
+
+@RestController
+public class MyController {
+
+    @PostMapping(value = "/feedback")
+    public ResponseEntity<Response> feedback(@RequestBody Request request){
+
+        Response response = Response.builder()
+                .uid(request.getUid())
+                .operationUid(request.getOperationUid())
+                .systemTime(request.getSystemTime())
+                .code("success")
+                .errorCode("")
+                .errorMessage("")
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
---
+++ Lab_2/src/main/java/ru/alexey4he/lab_2/model/Request.java
@@ -0,0 +1,26 @@
+package ru.alexey4he.lab_2.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Request {
+
+    @NotBlank
+    private String uid;
+
+    private String operationUid;
+    private String systemName;
+    private String systemTime;
+    private String source;
+    private int communicationId;
+    private int templateId;
+    private int productCode;
+    private int smsCode;
+}
