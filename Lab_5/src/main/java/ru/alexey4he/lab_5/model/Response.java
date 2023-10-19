5669bf84e9d60f7f9d7a25876647925883e10a12
Alexey Che <alexey4he@yandex.ru>
2023-10-19 22:32:21 +0500

    TEst in Lab 5

--- Lab_5/src/main/java/ru/alexey4he/lab_5/model/Response.java
+++ Lab_5/src/main/java/ru/alexey4he/lab_5/model/Response.java
@@ -6,12 +6,33 @@ import lombok.Data;
 @Data
 @Builder
 public class Response {
+    /***
+     * Индвидуальный идентификатор сообщения
+     */
     private String uid;
+    /***
+     * Индвидуальный идентификатор операции
+     */
     private String operationUid;
+    /***
+     * Время формирования ответа
+     */
     private String systemTime;
+    /***
+     * Статус обработки запроса
+     */
     private Codes code;
+    /***
+     * Премия получаемая сотрудником
+     */
     private Double bonus;
+    /***
+     * Код ошибки
+     */
     private ErrorCodes errorCode;
+    /***
+     * Текстовое сообщение об ошибке
+     */
     private ErrorMessages errorMessage;
 
 }
