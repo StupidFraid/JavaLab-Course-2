d3dc1c8ec24685258860e1749ad2072d92a0782a
Alexey Che <alexey4he@yandex.ru>
2023-10-27 00:06:39 +0500

    Fix Error Lab_6

--- Lab_6/src/main/java/ru/alexey4he/Lab_6/model/DisciplineResponse.java
+++ Lab_6/src/main/java/ru/alexey4he/Lab_6/model/DisciplineResponse.java
@@ -1,7 +1,9 @@
 package ru.alexey4he.Lab_6.model;
 
+import lombok.Data;
 import ru.alexey4he.Lab_6.entity.Discipline;
 
+@Data
 public class DisciplineResponse {
     private Discipline discipline;
     private StatusCode statusCode;
